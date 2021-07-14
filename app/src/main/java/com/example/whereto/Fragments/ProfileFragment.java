97e52de8c2a49e615c19cbabbfc1eff81a843878
pau-cardoso <A01701490@itesm.mx>
2021-07-14 15:15:35 -0500

    Add logout

--- app/src/main/java/com/example/whereto/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/whereto/Fragments/ProfileFragment.java
@@ -1,16 +1,25 @@
 package com.example.whereto.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.whereto.LoginActivity;
 import com.example.whereto.R;
+import com.parse.ParseUser;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -20,6 +29,11 @@ import org.jetbrains.annotations.NotNull;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+    
+    public static final String TAG = "ProfileFragment";
+    
+    Button btnLogout;
+    Toolbar tbProfile;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -35,5 +49,29 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull @NotNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        /* Toolbar setup */
+        tbProfile = view.findViewById(R.id.tbProfile);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(tbProfile);
+
+        /* Button setup */
+        btnLogout = view.findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick logout button");
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+                Log.i(TAG, "currentUser: " + currentUser);
+                goLoginActivity();
+            }
+        });
+    }
+
+    private void goLoginActivity() {
+        Log.i(TAG, "Entered goLoginActivity");
+        Intent i = new Intent(getContext(), LoginActivity.class);
+        startActivity(i);
+        getActivity().finish();
     }
 }
