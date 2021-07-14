a0fa2be101f23168cfa9cee3bd7c479bbe0efea0
pau-cardoso <A01701490@itesm.mx>
2021-07-14 15:16:17 -0500

    Add signup

--- app/src/main/java/com/example/whereto/SignupActivity.java
+++ app/src/main/java/com/example/whereto/SignupActivity.java
@@ -2,13 +2,73 @@ package com.example.whereto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignupActivity extends AppCompatActivity {
+
+    public static final String TAG = "SignupActivity";
+
+    EditText etUsernameS;
+    EditText etPasswordS;
+    Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        etUsernameS = findViewById(R.id.etUsernameS);
+        etPasswordS = findViewById(R.id.etPasswordS);
+        btnSignup = findViewById(R.id.btnSignup);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick signup button");
+                String username = etUsernameS.getText().toString();
+                String password = etPasswordS.getText().toString();
+
+                // Create the ParseUser
+                ParseUser user = new ParseUser();
+                if (username != "" && password != "") {
+                    // Set core properties
+                    user.setUsername(username);
+                    user.setPassword(password);
+                } else {
+                    Toast.makeText(SignupActivity.this, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
+                }
+
+                // Invoke signUpInBackground
+                user.signUpInBackground(new SignUpCallback() {
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            goFeedActivity();
+                        } else {
+                            // Sign up didn't succeed. Look at the ParseException
+                            // to figure out what went wrong
+                            Log.e("TAG", "Signup failed", e);
+                            Toast.makeText(SignupActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void goFeedActivity() {
+        Log.i(TAG, "Entered goFeedActivity");
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
