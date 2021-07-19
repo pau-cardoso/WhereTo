6e04fac414a19380c1c09142d01a3dac38683839
pau-cardoso <A01701490@itesm.mx>
2021-07-19 12:09:08 -0500

    Fix action bar in Profile for logout

--- app/src/main/java/com/example/whereto/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/whereto/Fragments/ProfileFragment.java
@@ -13,6 +13,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -40,6 +41,28 @@ public class ProfileFragment extends Fragment {
     }
 
     @Override
+    public void onCreateOptionsMenu(@NonNull @NotNull Menu menu, @NonNull @NotNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        // Inflate the menu; this adds items to the action bar.
+        inflater.inflate(R.menu.profile_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull @NotNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.btnLogout1:
+                Log.i(TAG, "onClick logout button");
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+                Log.i(TAG, "currentUser: " + currentUser);
+                goLoginActivity();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
@@ -50,22 +73,12 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull @NotNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        /* Toolbar setup */
-        tbProfile = view.findViewById(R.id.tbProfile);
-        ((AppCompatActivity) getActivity()).setSupportActionBar(tbProfile);
-
-        /* Button setup */
-        btnLogout = view.findViewById(R.id.btnLogout);
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "onClick logout button");
-                ParseUser.logOut();
-                ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
-                Log.i(TAG, "currentUser: " + currentUser);
-                goLoginActivity();
-            }
-        });
+        /* Toolbar setup for Action Bar */
+        tbProfile = view.findViewById(R.id.tbProfile); // Finds the toolbar component
+        ((AppCompatActivity) getActivity()).setSupportActionBar(tbProfile); // Sets the toolbar as action bar
+        setHasOptionsMenu(true); // Sets the action bar for options on menu
+        //((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false); // Disables the showing of the title
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Profile"); // Changes action bar's title
     }
 
     private void goLoginActivity() {
