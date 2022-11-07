90b3048c4e73cf285931fadc3182dbcebb3bf221
Pau Cardoso <paulinacardoso7@gmail.com>
2022-11-06 18:14:31 -0600

    add missing files

---
+++ app/src/main/java/com/example/whereto/Activities/LogoScreen.java
@@ -0,0 +1,16 @@
+package com.example.whereto.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.whereto.R;
+
+public class LogoScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo_screen);
+    }
+}
